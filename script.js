5a9779851d99e80dcc5a0fcfc513fca1e01bfa91
boram-han <hanboram90@gmail.com>
2022-04-20 00:57:05 +0900

    second commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// 메뉴창기능
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+// 롤링텍스트 효과
+const pTag1 = document.querySelector('.first_parallel')
+
+const textArr1 = 'ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION'.split(' ')
+
+function initTexts(element, textArray) {
+    textArray.push(...textArray) 
+    for (let i = 0; i < textArray.length; i++) {
+        element.innerText += `${textArray[i]}\u00a0\u00a0`
+    }
+}
+
+initTexts(pTag1, textArr1)
+
+let count1 = 0
+
+function marqueeText(count, element, direction) {
+    if (count > element.scrollwidth / 2) {
+        element.style.transform = `translateX(0)`
+        count = 0
+    }
+    element.style.transform = `translateX(${count * direction}px)`
+    return count
+}
+
+function animate() {
+    count1++
+
+    count1 = marqueeText(count1, pTag1, -1)
+
+    window.requestAnimationFrame(animate)
+}
+
+animate()
+
+// 서치바기능
+function searchToggle(obj, evt){
+    var container = $(obj).closest('.search-wrapper');
+        if(!container.hasClass('active')){
+            container.addClass('active');
+            evt.preventDefault();
+        }
+        else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+            container.removeClass('active');
+            // clear input
+            container.find('.search-input').val('');
+        }
+}
