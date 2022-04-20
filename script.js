d6a0dd266947dd40041d84613381e78e810fb56d
boram-han <hanboram90@gmail.com>
2022-04-20 14:26:41 +0900

    에러수정

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 메뉴창기능
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
@@ -55,62 +54,4 @@ function searchToggle(obj, evt){
             // clear input
             container.find('.search-input').val('');
         }
-=======
-// 메뉴창기능
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
-
-// 롤링텍스트 효과
-const pTag1 = document.querySelector('.first_parallel')
-
-const textArr1 = 'ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION ON FAIRBITION'.split(' ')
-
-function initTexts(element, textArray) {
-    textArray.push(...textArray) 
-    for (let i = 0; i < textArray.length; i++) {
-        element.innerText += `${textArray[i]}\u00a0\u00a0`
-    }
-}
-
-initTexts(pTag1, textArr1)
-
-let count1 = 0
-
-function marqueeText(count, element, direction) {
-    if (count > element.scrollwidth / 2) {
-        element.style.transform = `translateX(0)`
-        count = 0
-    }
-    element.style.transform = `translateX(${count * direction}px)`
-    return count
-}
-
-function animate() {
-    count1++
-
-    count1 = marqueeText(count1, pTag1, -1)
-
-    window.requestAnimationFrame(animate)
-}
-
-animate()
-
-// 서치바기능
-function searchToggle(obj, evt){
-    var container = $(obj).closest('.search-wrapper');
-        if(!container.hasClass('active')){
-            container.addClass('active');
-            evt.preventDefault();
-        }
-        else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
-            container.removeClass('active');
-            // clear input
-            container.find('.search-input').val('');
-        }
->>>>>>> 30c21e8fda28400cf6eac28e51fb8d5fd82dfd60
 }
