234091b87f5ae9dbd89be99ac7b3e98ebc205c2d
꼼꼼한 원칙주의자, Rami <99344397+boram-han@users.noreply.github.com>
2022-04-30 19:11:00 +0900

    메인뼈대, 푸터완료
    - 전체적인 메인뼈대
    - 메인 슬라이드 진행중
    - 컨텐츠1~4 진행중
    - 푸터 완료

--- script.js
+++ script.js
@@ -41,17 +41,3 @@ function animate() {
 }
 
 animate()
-
-// 서치바기능
-function searchToggle(obj, evt){
-    var container = $(obj).closest('.search-wrapper');
-        if(!container.hasClass('active')){
-            container.addClass('active');
-            evt.preventDefault();
-        }
-        else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
-            container.removeClass('active');
-            // clear input
-            container.find('.search-input').val('');
-        }
-}
